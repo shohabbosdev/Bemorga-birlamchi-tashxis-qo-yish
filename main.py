e1d4b3b9394cdc4bf2eff429adeb7665b55d6343
Shokh Abbos <ushohabbos@gmail.com>
2024-10-21 22:12:23 +0500

    5 ta bo'lim qo'shildi ularning simptom natijalari chap bo'limda chiqadigan qilindi

--- main.py
+++ main.py
@@ -56,14 +56,13 @@ patient_names = [f"{item['familiya']} {item['ism']} {item['sharifi']}" for item 
 # UI for patient diagnosis
 st.title("💥 Экспертная система ранней диагностики")
 
+selected_name = st.selectbox("Выберите пациента", patient_names)
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["💥 Экспертная система ранней диагностики", "🥼 Атака артрита", "🧪 Тесты на РФ и АЦЦП","📉 Показательные Анализы", "📄 Переносимые процессы и\или болезни"])
 
-col1, col2 = st.columns(2)
-with col1:
-    with col1.container(border=True):
+with tab1:
+    with st.container(border=True):
         # Patient information (select from JSON file)
-        selected_name = st.selectbox("Выберите пациента", patient_names)
         patient = Patient(selected_name)
-
         # Checkbox inputs for each symptom
         st.subheader("Выберите симптомы пациента")
         symptoms_list = [
@@ -130,9 +129,218 @@ with col1:
         calc.add_illness(illness2)
         calc.add_illness(illness3)
     
+with tab2:
+    st.header("🥼 Атака артрита")
+    with st.container(border=True):
+        # Patient information (select from JSON file)
+        patient = Patient(selected_name)
+        # Checkbox inputs for each symptom
+        st.subheader("Выберите симптомы пациента")
+        symptoms_list = [
+            Symptom("Достижение максимальных проявлений артрита во время атаки за один день", "x1"),
+            Symptom("Одна или более атак артрита в анамнезе", "x2"),
+            Symptom("Периферический артрит (несимметричный, олигоартрит)", "x3")
+        ]
+
+        # Checkbox for each symptom
+        for symptom in symptoms_list:
+            selected = st.checkbox(symptom.name)
+            patient.add_symptom(symptom.name, "+" if selected else "-")
+
+        # Define Illnesses and their reference symptoms
+        illness1 = Illness("Ревматоидный артрит")
+        illness2 = Illness("Подагрический артрит")
+        illness3 = Illness("Реактивный артрит")
+
+        # Add symptoms to the illnesses based on reference answers
+        illness1.add_symptom(symptoms_list[0], "+")
+        illness1.add_symptom(symptoms_list[1], "+")
+        illness1.add_symptom(symptoms_list[2], "-")
+
+        illness2.add_symptom(symptoms_list[0], "+")
+        illness2.add_symptom(symptoms_list[1], "+")
+        illness2.add_symptom(symptoms_list[2], "-")
+
+        illness3.add_symptom(symptoms_list[0], "-")
+        illness3.add_symptom(symptoms_list[1], "-")
+        illness3.add_symptom(symptoms_list[2], "+")
+
+        # Perform calculation
+        calc = Calculation(patient)
+        calc.add_illness(illness1)
+        calc.add_illness(illness2)
+        calc.add_illness(illness3)
+        
+with tab3:
+    st.header("🧪 Тесты на РФ и АЦЦП")
+    with st.container(border=True):
+        # Patient information (select from JSON file)
+        patient = Patient(selected_name)
+        # Checkbox inputs for each symptom
+        st.subheader("Выберите симптомы пациента")
+        symptoms_list = [
+            Symptom("Отрицательны", "x1"),
+            Symptom("Слабо позитивны для РФ или АЦЦП (превысили границу нормы, но менее, чем в 3 раза)", "x2"),
+            Symptom("Высоко позитивны для РФ или АЦЦП (более, чем в 3 раза превысили границу нормы)", "x3")
+        ]
+
+        # Checkbox for each symptom
+        for symptom in symptoms_list:
+            selected = st.checkbox(symptom.name)
+            patient.add_symptom(symptom.name, "+" if selected else "-")
+
+        # Define Illnesses and their reference symptoms
+        illness1 = Illness("Ревматоидный артрит")
+        illness2 = Illness("Подагрический артрит")
+        illness3 = Illness("Реактивный артрит")
+
+        # Add symptoms to the illnesses based on reference answers
+        illness1.add_symptom(symptoms_list[0], "-")
+        illness1.add_symptom(symptoms_list[1], "+")
+        illness1.add_symptom(symptoms_list[2], "+")
+
+        illness2.add_symptom(symptoms_list[0], "-")
+        illness2.add_symptom(symptoms_list[1], "-")
+        illness2.add_symptom(symptoms_list[2], "-")
+
+        illness3.add_symptom(symptoms_list[0], "-")
+        illness3.add_symptom(symptoms_list[1], "-")
+        illness3.add_symptom(symptoms_list[2], "-")
+
+        # Perform calculation
+        calc = Calculation(patient)
+        calc.add_illness(illness1)
+        calc.add_illness(illness2)
+        calc.add_illness(illness3)
+
+with tab4:
+    st.header("📉 Показательные Анализы")
+    with st.container(border=True):
+        # Patient information (select from JSON file)
+        patient = Patient(selected_name)
+        # Checkbox inputs for each symptom
+        st.subheader("Выберите симптомы пациента")
+        symptoms_list = [
+            Symptom("Норма по СОЭ ( Скорость оседания эритроцитов) и СРБ (С-реактивный белок)", "x1"),
+            Symptom("Повышение СОЭ или уровня СРБ", "x2"),
+            Symptom("Мочевая кислота >6,0 мг/дл (360 мкмоль/л)", "x3")
+        ]
+
+        # Checkbox for each symptom
+        for symptom in symptoms_list:
+            selected = st.checkbox(symptom.name)
+            patient.add_symptom(symptom.name, "+" if selected else "-")
+
+        # Define Illnesses and their reference symptoms
+        illness1 = Illness("Ревматоидный артрит")
+        illness2 = Illness("Подагрический артрит")
+        illness3 = Illness("Реактивный артрит")
+
+        # Add symptoms to the illnesses based on reference answers
+        illness1.add_symptom(symptoms_list[0], "-")
+        illness1.add_symptom(symptoms_list[1], "+")
+        illness1.add_symptom(symptoms_list[2], "-")
+
+        illness2.add_symptom(symptoms_list[0], "-")
+        illness2.add_symptom(symptoms_list[1], "-")
+        illness2.add_symptom(symptoms_list[2], "+")
+
+        illness3.add_symptom(symptoms_list[0], "-")
+        illness3.add_symptom(symptoms_list[1], "-")
+        illness3.add_symptom(symptoms_list[2], "-")
+
+        # Perform calculation
+        calc = Calculation(patient)
+        calc.add_illness(illness1)
+        calc.add_illness(illness2)
+        calc.add_illness(illness3)
+
+with tab5:
+    st.header("📄 Переносимые процессы и\или болезни")
+    with st.container(border=True):
+        # Patient information (select from JSON file)
+        patient = Patient(selected_name)
+        # Checkbox inputs for each symptom
+        st.subheader("Выберите симптомы пациента")
+        symptoms_list = [
+            Symptom("Синовит< 6 нед.", "x1"),
+            Symptom("Синовит >=6 нед.", "x2"),
+            Symptom("Гипертензия и/или одна или более кардиоваскулярных болезней", "x3"),
+            Symptom("Конъюнктивит", "x4"),
+            Symptom("Уретрит, простатит", "x5"),
+            Symptom("Эндоскопические признаки поражения кишечника", "x6"),
+            Symptom("Бленнорагическая кератодермия", "x7"),
+            Symptom("Эрозивный круговидный баланит (4–20%)", "x8"),
+            Symptom("Язвы слизистой оболочки полости рта", "x9"),
+            Symptom("Гиперкератоз ногтей", "x10"),
+            Symptom("Нарушения проводимости по данным ЭКГ", "x11"),
+            Symptom("Мужской пол", "x12"),
+            Symptom("Женский пол", "x13")
+        ]
+
+        # Checkbox for each symptom
+        for symptom in symptoms_list:
+            selected = st.checkbox(symptom.name)
+            patient.add_symptom(symptom.name, "+" if selected else "-")
+
+        # Define Illnesses and their reference symptoms
+        illness1 = Illness("Ревматоидный артрит")
+        illness2 = Illness("Подагрический артрит")
+        illness3 = Illness("Реактивный артрит")
+
+        # Add symptoms to the illnesses based on reference answers
+        illness1.add_symptom(symptoms_list[0], "-")
+        illness1.add_symptom(symptoms_list[1], "+")
+        illness1.add_symptom(symptoms_list[2], "-")
+        illness1.add_symptom(symptoms_list[3], "-")
+        illness1.add_symptom(symptoms_list[4], "-")
+        illness1.add_symptom(symptoms_list[5], "-")
+        illness1.add_symptom(symptoms_list[6], "-")
+        illness1.add_symptom(symptoms_list[7], "-")
+        illness1.add_symptom(symptoms_list[8], "-")
+        illness1.add_symptom(symptoms_list[9], "-")
+        illness1.add_symptom(symptoms_list[10], "-")
+        illness1.add_symptom(symptoms_list[11], "-")
+        illness1.add_symptom(symptoms_list[12], "-")
+
+        illness2.add_symptom(symptoms_list[0], "-")
+        illness2.add_symptom(symptoms_list[1], "-")
+        illness2.add_symptom(symptoms_list[2], "+")
+        illness2.add_symptom(symptoms_list[3], "-")
+        illness2.add_symptom(symptoms_list[4], "-")
+        illness2.add_symptom(symptoms_list[5], "-")
+        illness2.add_symptom(symptoms_list[6], "-")
+        illness2.add_symptom(symptoms_list[7], "-")
+        illness2.add_symptom(symptoms_list[8], "-")
+        illness2.add_symptom(symptoms_list[9], "-")
+        illness2.add_symptom(symptoms_list[10], "-")
+        illness2.add_symptom(symptoms_list[11], "-")
+        illness2.add_symptom(symptoms_list[12], "-")
+
+        illness3.add_symptom(symptoms_list[0], "-")
+        illness3.add_symptom(symptoms_list[1], "-")
+        illness3.add_symptom(symptoms_list[2], "-")
+        illness3.add_symptom(symptoms_list[3], "+")
+        illness3.add_symptom(symptoms_list[4], "+")
+        illness3.add_symptom(symptoms_list[5], "+")
+        illness3.add_symptom(symptoms_list[6], "+")
+        illness3.add_symptom(symptoms_list[7], "+")
+        illness3.add_symptom(symptoms_list[8], "+")
+        illness3.add_symptom(symptoms_list[9], "+")
+        illness3.add_symptom(symptoms_list[10], "+")
+        illness3.add_symptom(symptoms_list[11], "-")
+        illness3.add_symptom(symptoms_list[12], "-")
+
+        # Perform calculation
+        calc = Calculation(patient)
+        calc.add_illness(illness1)
+        calc.add_illness(illness2)
+        calc.add_illness(illness3)
+
+
 import random
-with col2:
-    with col2.container(border=True):
+with st.sidebar:
+    with st.container(border=True):
         if st.button("Рассчитать результат", icon="🗝"):
             results = calc.calculate()
             st.subheader("Результаты диагностики")
